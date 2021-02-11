a7f73bd21ac54a56f4e532870d33d5e54c9e1e45
TimasTT <timofey.konoplyov@gmail.com>
2021-02-11 22:06:22 +0300

    +din_array (yourself version) +vector (yourself version) +length of Hemming algorithm

--- hemming/main.cpp
+++ hemming/main.cpp
@@ -1,19 +1,103 @@
 #include <iostream>
+#include <cstring>
 
-int main() {
-    char a[] = "dsfafs";
-    char b[] = "asfafd";
-    int hem_cnt = 0;
+class string {
+    char *data;
+    int size;
+    int capacity;
+public:
+    explicit string(size_t s = 0, int c = 0, char *a = nullptr):size(s), capacity(c), data(a) {};
 
-    if (sizeof (a) == sizeof (b)) {
-        for (int i = 0; i < 6; ++i) {
-            if (a[i] != b[i]) {
-                hem_cnt += 1;
-            }
+    explicit string(int size) {
+        this->size = size;
+        this->capacity = size;
+        if (size != 0) {
+            data = new char(size);
+        } else {
+            data = nullptr;
         }
     }
 
-    std::cout << hem_cnt << std::endl;
+    string(const char *a) {
+        char *new_data = new char(strlen(a) + 1);
+        strcpy(new_data, a);
+        size = strlen(a);
+        capacity = strlen(a);
+        delete [] data;
+        data = new_data;
+    }
 
+    string& operator= (const char* a) {
+        delete [] data;
+        data = new char(strlen(a) + 1);
+        strcpy(data,a);
+        size = strlen(a);
+        capacity = strlen(a);
+        return *this;
+    }
+
+    ~string() {
+        delete [] data;
+    }
+
+    void resize(int size) {
+        if (size > capacity) {
+            capacity += 10;
+            char *new_data = new char(size);
+            for (int i = 0; i < this->size; ++i) {
+                new_data[i] = data[i];
+            }
+            delete [] data;
+            data = new_data;
+            this->size = size;
+        }
+        this->size = size;
+    }
+
+    void push_back(int item) {
+        resize(this->size + 1);
+        data[this->size - 1] = item;
+    }
+
+    void show() {
+        for (int i = 0; i < size; ++i) {
+            std::cout << data[i];
+        }
+        std::cout << std::endl;
+    }
+
+    int show_size() {
+        return size;
+    }
+
+    char & operator[] (const int i) {
+        return data[i];
+    }
+};
+
+std::ostream& operator << (std::ostream & out, int *data) {
+    out << *data;
+    return out;
+}
+
+int hemming(string &a, string &b) {
+    int cnt = 0;
+    if (a.show_size() == b.show_size()) {
+        for (int i = 0; i < a.show_size(); ++i) {
+            if (a[i] != b[i]) {
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    const char b[] = "dsdsfs" ;
+    string a(b);
+    a.show();
+    string c = "dsaaaa";
+    c.show();
+    std::cout << hemming(a, c) << std::endl;
     return 0;
 }
